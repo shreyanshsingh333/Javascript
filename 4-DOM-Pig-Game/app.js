f203f508da1b0a9cf6026977d836c3579373f42b
shreyansh singh <shreyanshsingh333@gmail.com>
2018-12-26 04:11:52 +0530

    minor updates

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -12,17 +12,67 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
+/*
 dice = Math.floor(Math.random() * 6) + 1;
 
-/* querySelector to write to the DOM */
+//  querySelector to write to the DOM
 document.querySelector("#current-" + activePlayer).textContent = dice;
 // document.querySelector("#current-" + activePlayer).innerHTML =
 //   "<em>" + dice + "</em>";
 
-/* querySelector to read from the DOM */
+// querySelector to read from the DOM
 var x = document.querySelector("#score-0").textContent;
 console.log(x);
+*/
 
 document.querySelector(".dice").style.display = "NONE";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+/*
+ // function callback method
+
+function btn(){
+    // do something here
+}
+
+document.querySelector('.btn-roll').addEventListener('click', btn);
+*/
+
+//-----------------------------------------------------------
+/* using anonymous function */
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  // 1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+  // 2. Display the results
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+  // 3. update the round score IF the rolled number is NOT a 1
+  if (dice !== 1) {
+    //Add score and display it in current
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    // Next player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    // document.querySelector(".player-0-panel").classList.remove("active");
+    // document.querySelector(".player-1-panel").classList.add("active");
+
+    // toggle active state
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    document.querySelector(".dice").style.display = "none";
+  }
+});
