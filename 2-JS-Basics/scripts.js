6f924d62ce0b8bad093c9cf07b5cd1dadc44f26c
shreyansh singh <shreyanshsingh333@gmail.com>
2018-12-26 02:37:01 +0530

    minor changes

---
+++ 2-JS-Basics/scripts.js
@@ -0,0 +1,20 @@
+// Lecture: variables
+/*
+var name = "John";
+console.log(name);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+// Lecture: variables 2
+var name = "John";
+var age = 26;
+console.log(name + age);
+console.log(age + age);
+
+var lastName = prompt("What is the last name?");
+console.log(lastName);
